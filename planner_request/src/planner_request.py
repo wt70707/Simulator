c70709eef782548c11fab84e62d6ed6d886808db
rohan <rohan.bhargava11@gmail.com>
2014-06-09 15:43:33 -0300

    goal can be sent by script and we can get the feedback(state, goal status) as well

--- planner_request/src/planner_request.py
+++ planner_request/src/planner_request.py
@@ -1,35 +1,112 @@
 #!/usr/bin/env python
 import rospy
+import roslib
+roslib.load_manifest('action_controller')
 from moveit_commander import MoveGroupCommander
-from moveit_msgs.msg import Constraints, JointConstraint
-
-
-if __name__ =='__main__':
-	#con=Constraints()
-	#constriants are only possible in moveit_commander 0.5.6 --> indigo
-	#con.name='test'
-	#k=JointConstraint()
-	#k.position=2.0
-	#k.tolerance_above=1.0
-	#k.tolerance_below=1.0
-	#k.weight=1.0
-	#k.joint_name='virtual_join'
-	#k=['virtual_join',2.0,1.0,1.0,2.0]
-	#con.joint_constraints.append(k)	
-	#print con
-	#if type(con) is Constraints:
-	#	print 'succes'
-	#else:
-	#	print 'failure'
+from moveit_msgs.msg import Constraints, JointConstraint, RobotState
+from action_controller.msg import *
+import actionlib
+import time
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import Imu
+from geometry_msgs.msg import Pose, Transform
+class Staterobot():
+	def __init__(self,topic):
+		#self.position=[]
+		self.state=Odometry()
+		self.position=RobotState()
+		self.transform=Transform()
+		#return position
+		rospy.Subscriber(topic,Odometry,self.callback)
+	def callback(self,data):
+		#self.state=data.pose.pose.positio
+		self.transform.translation=data.pose.pose.position
+		self.transform.rotation=data.pose.pose.orientation		
+		self.state=[data.pose.pose.position.x,data.pose.pose.position.y,data.pose.pose.position.z,data.pose.pose.orientation.x,data.pose.pose.orientation.y,data.pose.pose.orientation.z,data.pose.pose.orientation.w]
+		if len(self.position.multi_dof_joint_state.transforms)==0:
+			self.position.multi_dof_joint_state.transforms.append(self.transform)
+			self.position.multi_dof_joint_state.joint_names.append('virtual_join')
+		else:
+			self.position.multi_dof_joint_state.transforms=[]
+			self.position.multi_dof_joint_state.transforms.append(self.transform)
+			self.position.multi_dof_joint_state.joint_names=[]
+			self.position.multi_dof_joint_state.joint_names.append('virtual_join')
+		self.position.joint_state.header.frame_id='/nav'
+		self.position.multi_dof_joint_state.header.frame_id='/nav'
 	
+	def getposition(self):
+		return self.position	
+
+	def getstate(self):
+		return self.state
+
+obj=Staterobot('/ground_truth/state')
+
+def callback(data):
+	t=data.status
 	group=MoveGroupCommander('spiri')
 	group.set_planner_id('PRMkConfigDefault')
-	#group.set_path_constraints(con)	
-	#print conversions.msg_to_string(con)
-	#group.set_workspace([-2.0,10.0,-2.0,10.0,-2.0,10.0])
 	goal=group.get_current_joint_values()
-	#print group.get_planning_frame()	
-	goal[2]=2.0
-	#print goal
- 	group.set_joint_value_target(goal)
-	group.plan()
+	#print t.status	
+	if(t.status==3):
+		time.sleep(5)
+		print 'completed one goal'
+		start=obj.getposition()
+		group.set_start_state(start)
+	
+		#goal=group.get_current_joint_values()
+		print start
+		goal=obj.getstate()
+		#group.set_start_state(goal)		
+		goal[0]=goal[0]+2.0
+		print goal
+		group.set_joint_value_target(goal)
+		plan=group.plan()
+		time.sleep(5.0)
+		group.execute(plan)
+		
+	if(t.staus==2):
+		print 'timeout'
+	
+def callback_imu(data):
+	global obj
+	print obj.getstate()
+
+
+def listener():
+	
+	rospy.init_node('spiri_goal',anonymous=True)
+	
+	#print obj.getstate()
+	rospy.Subscriber('multi_dof_joint_trajectory_action/result',MultiDofFollowJointTrajectoryActionResult,callback)
+	#rospy.Subscriber('/raw_imu',Imu,callback_imu)	
+	try:
+	
+		rospy.spin()
+	except KeyboardInterrupt:
+		print 'shutting down'
+
+
+if __name__=='__main__':
+	listener()
+	#group=MoveGroupCommander('spiri')
+	#group.set_planner_id('PRMkConfigDefault')
+	#goal=obj.getstate()
+	#goal_2=RobotState()
+	
+	#t=Transform()
+	#t.translation.z=1.0
+	#goal_2.multi_dof_joint_state.transforms.append(t)
+	#goal_2.multi_dof_joint_state.joint_names.append('virtual_join')
+	#goal_2.multi_dof_joint_state.header.frame_id='/nav'
+	#goal_2.joint_state.header.frame_id='/nav'
+	#print goal_2
+ 	#print group.get_random_joint_values()	
+	#group.set_start_state(goal_2)
+	#goal=group.get_current_joint_values()
+	#goal[0]=1.0
+	#goal[2]=1.0
+	#group.set_joint_value_target(goal)
+	#group.plan()
+		
+		
