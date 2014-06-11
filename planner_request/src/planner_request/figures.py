45a7feabe2e528b3a16de6704a7731ade7a837c8
rohan <rohan.bhargava11@gmail.com>
2014-06-11 16:37:08 -0300

    added code for vertical movement

--- planner_request/src/planner_request/figures.py
+++ planner_request/src/planner_request/figures.py
@@ -6,11 +6,29 @@ import numpy as np
 		
 def square_compute(start,diag_start):
 	waypoints=[]
-	waypoints=[(start[0],diag_start[1]),(diag_start[0],diag_start[1]),(diag_start[0],start[1]),(start[0],start[1])]
+	waypoints=[(start[0],diag_start[1],start[2]),(diag_start[0],diag_start[1],start[2]),(diag_start[0],start[1],start[2]),(start[0],start[1],start[2])]
 	return waypoints	
+
+def square_compute_vertical(start,diag_start,axis='x'):
+	waypoints=[]
+	if axis=='x':
+
+		waypoints=[(start[0],start[1],diag_start[2]),(diag_start[0],start[1],diag_start[2]),(diag_start[0],start[1],start[2]),(start[0],start[1],start[2])]
+	if axis=='y':
+		waypoints=[(start[0],start[1],diag_start[2]),(start[0],diag_start[1],diag_start[2]),(start[0],diag_start[1],start[2]),(start[0],start[1],start[2])]
+	return waypoints
 def triangle_compute(start,height,base):
 	waypoints=[]
-	waypoints=[(start[0]+base/2,start[1]+height),(start[0]+base,start[1]),(start[0],start[1])]
+	waypoints=[(start[0]+base/2,start[1]+height,start[2]),(start[0]+base,start[1],start[2]),(start[0],start[1],start[2])]
+	return waypoints
+
+def triangle_compute_vertical(start,height,base,axis='x'):
+	waypoints=[]
+	if axis=='x':
+	
+		waypoints=[(start[0]+base/2,start[1],start[2]+height),(start[0]+base,start[1],start[2]),(start[0],start[1],start[2])]
+	if axis=='y':
+		waypoints=[(start[0],start[1]+base/2,start[2]+height),(start[0],start[1]+base,start[2]),(start[0],start[1],start[2])]		
 	return waypoints
 
 def horizontal_sine_wave(start,base=np.pi):
