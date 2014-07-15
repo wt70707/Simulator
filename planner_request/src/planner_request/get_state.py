ff23b7806f60a7cc3b558fca243736dda633b2fd
rohanbhargava11 <rohan.bhargava11@gmail.com>
2014-07-15 16:37:04 -0300

    removed some comments

--- planner_request/src/planner_request/get_state.py
+++ planner_request/src/planner_request/get_state.py
@@ -6,11 +6,9 @@ from geometry_msgs.msg import PoseStamped
 class Staterobot():
 	def __init__(self,topic):
 		self.state=PoseStamped()
-		#self.topic=topic
-		#return position
 		rospy.Subscriber(topic,Odometry,self.callback)
 	def callback(self,data):
-		#self.state=[data.pose.pose.position.x,data.pose.pose.position.y,data.pose.pose.position.z,data.pose.pose.orientation.x,data.pose.pose.orientation.y,data.pose.pose.orientation.z,data.pose.pose.orientation.w]
+		
 		self.state.pose=data.pose.pose
 		self.state.header=data.header
 	def getstate(self):
