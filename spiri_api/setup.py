1c860ae6666d45b9c4f2bf83516f14d0b1ceb20b
rob <rohan.bhargava11@gmail.com>
2014-08-04 09:34:58 -0300

    added spiri_create_pkg to bin

--- spiri_api/setup.py
+++ spiri_api/setup.py
@@ -4,5 +4,5 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup()
 d['packages'] = ['spiri_api']
 d['package_dir'] = {'': 'src'}
-
+d['scripts']=['bin/spiri_create_pkg']
 setup(**d)
