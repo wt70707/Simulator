958d02d2ee8c069a948cbe982a4cd2da0a9307f8
rob <rohan.bhargava11@gmail.com>
2014-08-06 14:55:05 -0300

    documentation should be uploaded

---
+++ search/classes_73.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['staterobot',['Staterobot',['../class_staterobot.html',1,'']]],
+  ['staterobot',['Staterobot',['../classspiri__api_1_1request_1_1_staterobot.html',1,'spiri_api::request']]],
+  ['staterobot',['Staterobot',['../classspiri__api_1_1figure__movement_1_1_staterobot.html',1,'spiri_api::figure_movement']]],
+  ['staterobot',['Staterobot',['../classspiri__api_1_1get__state_1_1_staterobot.html',1,'spiri_api::get_state']]]
+];
