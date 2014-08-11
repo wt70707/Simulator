75733c8447cd9a79334c2f989677c568415a10ae
Rohan Bhargava <rohan.bhargava11@gmail.com>
2014-08-11 17:40:04 -0300

    documentation for the python wrapper

---
+++ search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['_5f_5finit_5f_5f',['__init__',['../classspiri__api_1_1get__state_1_1_staterobot.html#a015efa1f3a59211063e167ec0cb1fc3b',1,'spiri_api::get_state::Staterobot']]]
+];
---
+++ search/functions_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['update',['update',['../classspiri__api_1_1pid_1_1_p_i_d.html#a7e0273463b18d2f0ddef7f91c8457ea9',1,'spiri_api::pid::PID']]]
+];
