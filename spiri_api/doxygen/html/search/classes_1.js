ce74611481369376dddea5f94a9c4c4a95335a91
Rohan Bhargava <rohan.bhargava11@gmail.com>
2014-08-11 17:36:10 -0300

    documentation for the python wrapper

---
+++ spiri_api/doxygen/html/search/classes_1.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['spiri_5fcamera_5finterface',['spiri_camera_interface',['../classspiri__api_1_1camera_1_1spiri__camera__interface.html',1,'spiri_api::camera']]],
+  ['staterobot',['Staterobot',['../classspiri__api_1_1request_1_1_staterobot.html',1,'spiri_api::request']]],
+  ['staterobot',['Staterobot',['../classspiri__api_1_1figure__movement_1_1_staterobot.html',1,'spiri_api::figure_movement']]],
+  ['staterobot',['Staterobot',['../classspiri__api_1_1get__state_1_1_staterobot.html',1,'spiri_api::get_state']]],
+  ['staterobot',['Staterobot',['../class_staterobot.html',1,'']]]
+];
---
+++ spiri_api/doxygen/html/search/functions_2.js
@@ -0,0 +1,22 @@
+var searchData=
+[
+  ['get_5fbottom_5fimage',['get_bottom_image',['../class_staterobot.html#a310b2c9315316f96054357ebb705438f',1,'Staterobot']]],
+  ['get_5fbottom_5fimage_5fpython',['get_bottom_image_python',['../class_staterobot.html#a36d188439ca66b3e56cff58877297945',1,'Staterobot']]],
+  ['get_5fgps_5fdata',['get_gps_data',['../class_staterobot.html#a31dcc23cfb620c95e58f3b5ad4bb6b6d',1,'Staterobot::get_gps_data()'],['../classspiri__api_1_1get__state_1_1_staterobot.html#a1e11c48e95eb0184924776fdbc55b45b',1,'spiri_api.get_state.Staterobot.get_gps_data()']]],
+  ['get_5fgps_5fdata_5fpython',['get_gps_data_python',['../class_staterobot.html#ac2fe6ca526d21c1bcb2e284f000759a4',1,'Staterobot']]],
+  ['get_5fgps_5fvel',['get_gps_vel',['../class_staterobot.html#a3a9ee7a49cbb278ffc5f3b201dcae951',1,'Staterobot::get_gps_vel()'],['../classspiri__api_1_1get__state_1_1_staterobot.html#af029d50c1f24e86ebfa2aa12d32d9501',1,'spiri_api.get_state.Staterobot.get_gps_vel()']]],
+  ['get_5fgps_5fvel_5fpython',['get_gps_vel_python',['../class_staterobot.html#aed3a2e6aa076fe6e38f97eb9ab9ef41a',1,'Staterobot']]],
+  ['get_5fheight_5faltimeter',['get_height_altimeter',['../class_staterobot.html#a3e92b9d6f17c0812e8ed435602d1f717',1,'Staterobot::get_height_altimeter()'],['../classspiri__api_1_1get__state_1_1_staterobot.html#ac0a65631dbe7edbf353799ca0faab408',1,'spiri_api.get_state.Staterobot.get_height_altimeter()']]],
+  ['get_5fheight_5fpressure',['get_height_pressure',['../class_staterobot.html#a0318f7abf24c5c60aee915224af6af02',1,'Staterobot::get_height_pressure()'],['../classspiri__api_1_1get__state_1_1_staterobot.html#a9a2667a1295e2b9731614271eb021299',1,'spiri_api.get_state.Staterobot.get_height_pressure()']]],
+  ['get_5fimu',['get_imu',['../class_staterobot.html#a50e002ec81917b295a87463127648a52',1,'Staterobot']]],
+  ['get_5fimu_5fpython',['get_imu_python',['../class_staterobot.html#a3e6bd1883c62204de398342cf8004351',1,'Staterobot']]],
+  ['get_5fleft_5fimage',['get_left_image',['../class_staterobot.html#a5b0323154aa0a19ed82b02cfe7376974',1,'Staterobot']]],
+  ['get_5fleft_5fimage_5fpython',['get_left_image_python',['../class_staterobot.html#ab7e02e341f1f3ccfaaa322a2fc1e2caf',1,'Staterobot']]],
+  ['get_5forientation',['get_orientation',['../classspiri__api_1_1get__state_1_1_staterobot.html#ab8646e99f523f709242d7079a184057e',1,'spiri_api::get_state::Staterobot']]],
+  ['get_5forientation_5fimu',['get_orientation_imu',['../classspiri__api_1_1get__state_1_1_staterobot.html#a841fe6f1d8d15f7c1aa3022eea33ccdd',1,'spiri_api::get_state::Staterobot']]],
+  ['get_5fposition',['get_position',['../classspiri__api_1_1get__state_1_1_staterobot.html#acaa2d63d3153f862a5752ebb75f51a23',1,'spiri_api::get_state::Staterobot']]],
+  ['get_5fright_5fimage',['get_right_image',['../class_staterobot.html#ab1d8f9bfdbd766b96110c0a65fe08f43',1,'Staterobot']]],
+  ['get_5fright_5fimage_5fpython',['get_right_image_python',['../class_staterobot.html#ab222942f5fe07919fc4999eb600706d1',1,'Staterobot']]],
+  ['get_5fstate',['get_state',['../class_staterobot.html#a51670cc44348eae6328f220421ea328f',1,'Staterobot']]],
+  ['get_5fstate_5fpython',['get_state_python',['../class_staterobot.html#a91206b2bef7c39193df2eef7b8bae523',1,'Staterobot']]]
+];
