0c16c9b19eee63aea44f038e7511539fa09f1e4d
rob <rohan.bhargava11@gmail.com>
2014-08-12 14:14:53 -0300

    updated documentation

--- search/all_63.js
+++ search/all_63.js
@@ -5,7 +5,5 @@ var searchData=
   ['callback_5fgps_5fvel',['callback_gps_vel',['../classspiri__api_1_1get__state_1_1_staterobot.html#a9a9c3e465592be5e619fe5a68a6563a0',1,'spiri_api::get_state::Staterobot']]],
   ['callback_5fimu',['callback_imu',['../classspiri__api_1_1get__state_1_1_staterobot.html#a9136ac9a32b6c7092b3f00ee0fddc3e8',1,'spiri_api::get_state::Staterobot']]],
   ['callback_5fpressure',['callback_pressure',['../classspiri__api_1_1get__state_1_1_staterobot.html#a7597952c4302e24b63fbbc2653221bc9',1,'spiri_api::get_state::Staterobot']]],
-  ['callback_5fstate',['callback_state',['../classspiri__api_1_1get__state_1_1_staterobot.html#a81570bdf94a8ea111eb9a51b3044c70d',1,'spiri_api::get_state::Staterobot']]],
-  ['callback_5fthreading',['callback_threading',['../classspiri__api_1_1camera_1_1camera__interface.html#aa01883759da88698cfa72cb7988167c7',1,'spiri_api::camera::camera_interface']]],
-  ['camera_5finterface',['camera_interface',['../classspiri__api_1_1camera_1_1camera__interface.html',1,'spiri_api::camera']]]
+  ['callback_5fstate',['callback_state',['../classspiri__api_1_1get__state_1_1_staterobot.html#a81570bdf94a8ea111eb9a51b3044c70d',1,'spiri_api::get_state::Staterobot']]]
 ];
---
+++ search/classes_6f.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['orientation',['orientation',['../struct_staterobot_1_1orientation.html',1,'Staterobot']]]
+];
