f068ed2fe09a2bfd677828a58372b996dd107d6e
rob <rohan.bhargava11@gmail.com>
2014-08-19 12:14:41 -0300

    check build folder ignored

--- spiri_apps/spiri_inspection/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rob/catkin_ws/src/Simulator/spiri_apps/spiri_inspection
-# Build directory: /home/rob/catkin_ws/src/Simulator/spiri_apps/spiri_inspection
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
