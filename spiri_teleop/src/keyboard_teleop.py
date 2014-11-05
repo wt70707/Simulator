03d61497d211e982cb054652c7e0dff2bd987719
rob <rohan.bhargava11@gmail.com>
2014-11-05 11:37:33 -0400

    added pose estimation and teleop code

--- spiri_teleop/src/keyboard_teleop.py
+++ spiri_teleop/src/keyboard_teleop.py
@@ -1,146 +1,136 @@
 #!/usr/bin/env python
-import roslib; roslib.load_manifest('spiri_teleop')
 import rospy
 import time
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 
 import sys, select, termios, tty
-from spiri_api import spiri_api_python
-spiri=spiri_api_python()
 
+class SpiriKeyboardTeleopNode:
+    def __init__(self):
+        self.settings = termios.tcgetattr(sys.stdin)
+        cmd_vel_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+        
+        state_topic = rospy.get_param("state_topic", "/ground_truth/state")
+        state_sub = rospy.Subscriber(state_topic, Odometry, self.state_callback, queue_size=1)
+        
+        self.hover_height = rospy.get_param("hover_height", 2)
+        self.hover_thresh = rospy.get_param("hover_thresh", 0.1)
+        
+        rospy.init_node('teleop_twist_keyboard')
+        self.state = Odometry()
+        
+        r = rospy.Rate(300)
+        key = ''
+        
+        while not rospy.is_shutdown():
+            cmd_vel = Twist()
+            if key == '':
+                key = self.get_key()
+            #print "command: " + str(key)
+            err = None
+            if key in self.moveBindings.keys():
+                if key == 'h':
+                    err = (self.hover_height - self.state.pose.pose.position.z)
+                    cmd_vel.linear.z = (self.hover_height - self.state.pose.pose.position.z)
+                
+                elif key=='n':
+                    err = -self.state.pose.pose.position.z
+                    cmd_vel.linear.z = err
+                    
+                else:
+                    cmd_vel.linear.x = self.moveBindings[key][0]
+                    cmd_vel.angular.z = self.moveBindings[key][1]
+                    cmd_vel.linear.z = self.moveBindings[key][2]
+                    
+            elif key in self.speedBindings.keys():
+				self.speed = self.speed * self.speedBindings[key][0]
+				self.turn = self.turn * self.speedBindings[key][1]
 
-msg = """
-Reading from the keyboard  and Publishing to Twist!
----------------------------
-Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
+				print "currently:\tspeed %s\tturn %s " % (self.speed, self.turn)
 
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
-p   : move up in z direction
-;   : move down in z directions
-h   : Hover
-n   : Land
-anything else : stop
+            else:
+                if (key == '\x03'):
+                    break
+                print self.msg
+            
+            if err == None or (abs(err) < self.hover_thresh):
+                key = ''
+            
+            cmd_vel.linear.x *= self.speed
+            cmd_vel.linear.y *= self.speed
+            cmd_vel.linear.z *= self.speed
+            
+            cmd_vel.angular.z *= self.turn
+            
+            cmd_vel_pub.publish(cmd_vel)
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)
+            r.sleep()
 
-CTRL-C to quit
-"""
+    def state_callback(self, msg):
+        #print msg
+        self.state = msg
 
-moveBindings = {
-		'i':(1,0,0),
-		'o':(1,-1,0),
-		'j':(0,1,0),
-		'l':(0,-1,0),
-		'u':(1,1,0),
-		',':(-1,0,0),
-		'.':(-1,1,0),
-		'm':(-1,-1,0),
-    		'p':(0,0,1),
-                ';':(0,0,-1),
-                'h':(0,0,0),
-                'n':(0,0,0),
-               }
+    def get_key(self):
+        tty.setraw(sys.stdin.fileno())
+        [rr, rw, rx] = select.select([sys.stdin], [], [], 0.25)
+        if sys.stdin in rr: 
+            key = sys.stdin.read(1)
+        else:
+             key = ""
+             
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.settings)             
+        return key
 
-speedBindings={
-		'q':(1.1,1.1),
-		'z':(.9,.9),
-		'w':(1.1,1),
-		'x':(.9,1),
-		'e':(1,1.1),
-		'c':(1,.9),
-	      }
+        
+    msg = """
+    Reading from the keyboard  and Publishing to Twist!
+    ---------------------------
+    Moving around:
+       u    i    o
+       j    k    l
+       m    ,    .
 
-def getKey():
-	tty.setraw(sys.stdin.fileno())
-	select.select([sys.stdin], [], [], 0)
-	key = sys.stdin.read(1)
-	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-	return key
+    q/z : increase/decrease max speeds by 10%
+    w/x : increase/decrease only linear speed by 10%
+    e/c : increase/decrease only angular speed by 10%
+    p   : move up in z direction
+    ;   : move down in z directions
+    h   : Hover
+    n   : Land
+    anything else : stop
 
-speed = .5
-turn = 1
+    CTRL-C to quit
+    """
 
-def vels(speed,turn):
-	return "currently:\tspeed %s\tturn %s " % (speed,turn)
+    moveBindings = {
+            '':(0,0,0),
+            'i':(1,0,0),
+            'o':(1,-1,0),
+            'j':(0,1,0),
+            'l':(0,-1,0),
+            'u':(1,1,0),
+            ',':(-1,0,0),
+            '.':(-1,1,0),
+            'm':(-1,-1,0),
+            'p':(0,0,1),
+            ';':(0,0,-1),
+            'h':(0,0,1),
+            'n':(0,0,0),
+    }
+
+    speedBindings={
+            'q':(1.1,1.1),
+            'z':(.9,.9),
+            'w':(1.1,1),
+            'x':(.9,1),
+            'e':(1,1.1),
+            'c':(1,.9),
+    }
+    
+    speed = .5
+    turn = 1        
 
 if __name__=="__main__":
-    	settings = termios.tcgetattr(sys.stdin)
+    spiri_keyboard_teleop_node = SpiriKeyboardTeleopNode()
 
-	pub = rospy.Publisher('cmd_vel', Twist)
-	rospy.init_node('teleop_twist_keyboard')
-
-	x = 0
-	th = 0
-	status = 0
-	z=0
-	
-	try:
-		print msg
-		print vels(speed,turn)
-		while(1):
-			state=spiri.get_state()
-			key = getKey()
-			if key in moveBindings.keys():
-				print key
-				if key=='h':
-				  
-				  if state.position.z<1.0:
-				    twist=Twist()
-				    twist.linear.z=1.0
-				    pub.publish(twist)
-				    time.sleep(1.0)
-				    twist=Twist()
-				    pub.publish(twist)
-				    x=moveBindings[key][0]
-				    th=moveBindings[key][1]
-				    z=moveBindings[key][2]
-				elif key=='n':
-				  twist=Twist()
-				  twist.linear.z=-state[2]
-				  pub.publish(twist)
-				  time.sleep(1.0)
-				  twist=Twist()
-				  pub.publish(twist)
-				  x=moveBindings[key][0]
-				  th=moveBindings[key][1]
-				  z=moveBindings[key][2]
-				      
-				else:
-				  x = moveBindings[key][0]
-				  th = moveBindings[key][1]
-				  #print moveBindings[key]
-				  z=moveBindings[key][2]
-			elif key in speedBindings.keys():
-				speed = speed * speedBindings[key][0]
-				turn = turn * speedBindings[key][1]
-
-				print vels(speed,turn)
-				if (status == 14):
-					print msg
-				status = (status + 1) % 15
-			else:
-				
-				x = 0
-				th = 0
-   				z=0
-				if (key == '\x03'):
-					break
-
-			twist = Twist()
-			twist.linear.x = x*speed; twist.linear.y = 0; twist.linear.z = z
-			twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = th*turn
-			pub.publish(twist)
-
-	except:
-		print 'error'
-
-	finally:
-		twist = Twist()
-		twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
-		twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
-		pub.publish(twist)
-
-    		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
