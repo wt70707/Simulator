c951bd9e8302bad1e8034a11a6088f026004cd13
Arnold Kalmbach <arnold.kalmbach@pleiades.ca>
2014-11-05 15:57:32 -0500

    Started testing collision warning node

--- collision_warning/src/collision_warning.cpp
+++ collision_warning/src/collision_warning.cpp
@@ -1,58 +1,75 @@
-#include <ros.h>
-#include <pcl_conversions.h>
+#include <iostream>
+#include <ros/ros.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <tf/listener.h>
-
+#include <geometry_msgs/Point.h>
+#include <tf/transform_listener.h>
 
 class PointCloudCollisionWarningNode
 {
-    PointCloudCollisionWarningNode(void) : transform_listener_(0)
-    {
-        nh.param<std::string>("point_cloud_topic", point_cloud_topic_name, "stereo/point_cloud");
-        point_cloud_sub_  = nh_.subscribe(point_cloud_topic_name, 1, &PointCloudCollisionWarningNode::pointCloudCallback, this);
-        nh.param<double>("warn_radius", warn_radius_, 1.0);
-    }
+    public:
+        PointCloudCollisionWarningNode(void);
     
     protected:
         ros::NodeHandle nh_;
-        tf::Listener transform_listener_;
+        tf::TransformListener tf_listener_;
         std::string point_cloud_topic_name;
         ros::Subscriber point_cloud_sub_;
         double warn_radius_;
-        
-
-        void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr point_cloud_msg)
-        {      
-            // Create a PCL point cloud object, in the frame of reference of the robot,
-            // so that any point within warn_radius_ of the origin is a potential danger point
-            pcl::PointCloud<pcl::PointXYZRGB> cloud;
-            pcl::PointCloud<pcl::PointXYZRGB> cloud_baselink_frame;
-            pcl::fromROSMsg(point_cloud_msg, cloud);
-            pcl_ros::transformPointCloud ("base_link", 0, cloud, point_cloud_msg->header.frame_id, cloud_baselink_frame, tf_listener_);
-
-            // Check for points within warn_radius_ of the origin
-            for (size_t i = 0; i < cloud->points.size (); ++i)
-            {
-                if ( sqrt(cloud_baselink_frame->points[i].x*cloud_baselink_frame->points[i].x +
-                          cloud_baselink_frame->points[i].y*cloud_baselink_frame->points[i].y +
-                          cloud_baselink_frame->points[i].z*cloud_baselink_frame->points[i].z) < warn_radius_
-                    )
-                    
-                    ROS_WARN("There is an obstacle at %f, %f, %f (%s frame)", 
-                        cloud_baselink_frame->points[i].x, cloud_baselink_frame->points[i].y, cloud_baselink_frame->points[i].z,
-                        "base_link" );
-                    
-                    // break;
-            }
-        }
-
+        void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr &point_cloud_msg);
 };
 
+PointCloudCollisionWarningNode::PointCloudCollisionWarningNode(void)
+{
+    nh_.param<std::string>("point_cloud_topic", point_cloud_topic_name, "stereo/points2");
+    point_cloud_sub_  = nh_.subscribe(point_cloud_topic_name, 1, &PointCloudCollisionWarningNode::pointCloudCallback, this);
+    nh_.param<double>("warn_radius", warn_radius_, 1.0);
+}
+
+
+void PointCloudCollisionWarningNode::pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr &point_cloud_msg)
+{      
+    // Create a PCL point cloud object, in the frame of reference of the robot,
+    // so that any point within warn_radius_ of the origin is a potential danger point
+    pcl::PointCloud<pcl::PointXYZRGB> cloud;
+    pcl::PointCloud<pcl::PointXYZRGB> cloud_baselink_frame;
+    pcl::fromROSMsg(*point_cloud_msg, cloud);
+    pcl_ros::transformPointCloud ("base_link", ros::Time::now(), cloud, point_cloud_msg->header.frame_id, cloud_baselink_frame, tf_listener_);
+    double min_dist = -1;
+    geometry_msgs::Point closest_point;
+
+    // Check for points within warn_radius_ of the origin
+    for (size_t i = 0; i < cloud_baselink_frame.points.size (); ++i)
+    {
+        double dist = sqrt(cloud_baselink_frame.points[i].x*cloud_baselink_frame.points[i].x +
+                            cloud_baselink_frame.points[i].y*cloud_baselink_frame.points[i].y +
+                            cloud_baselink_frame.points[i].z*cloud_baselink_frame.points[i].z );
+        if (min_dist < 0 || dist < min_dist)
+        {
+            min_dist = dist;
+            closest_point.x = cloud_baselink_frame.points[i].x;
+            closest_point.y = cloud_baselink_frame.points[i].y;
+            closest_point.z = cloud_baselink_frame.points[i].z;
+        }
+        if ( dist < warn_radius_ )
+        {
+            ROS_WARN("There is an obstacle at %f, %f, %f (%s frame)", 
+                cloud_baselink_frame.points[i].x, cloud_baselink_frame.points[i].y, cloud_baselink_frame.points[i].z,
+                "base_link" );
+            
+            // break;
+        }
+        ROS_INFO("Point @(%f, %f, %f) dist=%f", closest_point.x, closest_point.y, closest_point.z, dist);
+    }
+    //ROS_INFO("Closest point was at %f, %f, %f (%s frame)", closest_point.x, closest_point.y, closest_point.z, "base_link");
+}
+        
 int main(int argc, char* argv[])
 {
     ros::init(argc, argv, "point_cloud_safety");
-    PointCloudCollisionWarningNode pccwn();
     
+    PointCloudCollisionWarningNode pccwn;
     ros::spin();
 }
 
